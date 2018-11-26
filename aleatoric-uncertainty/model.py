8be500c1674d1a93cbb25e0785ee2f43209355f9
Pietro Morerio <pietro.morerio@iit.it>
2018-11-26 18:12:23 +0100

    no activation function

--- aleatoric-uncertainty/model.py
+++ aleatoric-uncertainty/model.py
@@ -48,28 +48,28 @@ class Model(object):
 		    
 		    mean = slim.conv2d_transpose(net, 256, [3, 3], scope='conv_transpose2_mean')   # (batch_size, 14, 14, 128)
 		    mean = slim.batch_norm(mean, scope='bn2_gen_mean')
-		    mean = slim.conv2d_transpose(mean, 1, [3, 3], activation_fn=tf.tanh, scope='conv_transpose3_mean')   # (batch_size, 28, 28, 1)
+		    mean = slim.conv2d_transpose(mean, 1, [3, 3], activation_fn=None, scope='conv_transpose3_mean')   # (batch_size, 28, 28, 1)
 		    
-		    std = slim.conv2d_transpose(net, 256, [3, 3], scope='conv_transpose2_std')   # (batch_size, 14, 14, 128)
-		    std = slim.batch_norm(std, scope='bn2_gen_std')
-		    std = slim.conv2d_transpose(std, 1, [3, 3], activation_fn=tf.nn.tanh, scope='conv_transpose3_std')   # (batch_size, 28, 28, 1)
+		    log_var = slim.conv2d_transpose(net, 256, [3, 3], scope='conv_transpose2_log_var')   # (batch_size, 14, 14, 128)
+		    log_var = slim.batch_norm(log_var, scope='bn2_gen_log_var')
+		    log_var = slim.conv2d_transpose(log_var, 1, [3, 3], activation_fn=None, scope='conv_transpose3_log_var')   # (batch_size, 28, 28, 1)
 		    # note: check if activation makes sense
 		    
-	return mean, 5.*std
+	return mean, log_var
     
 		    
     def build_model(self):
 	
 	print('[*] Building model')
 	self.images = tf.placeholder(tf.float32, [None, 28, 28, 1], 'images')
-	self.mean, self.std = self.EncoderDecoder(self.images, is_training = self.mode=='train')
+	self.mean, self.log_var = self.EncoderDecoder(self.images, is_training = self.mode=='train')
 
 	# loss
-	#~ self.loss = tf.reduce_mean( tf.square( (self.mean-self.images)/(self.std+1e-6) ) + tf.log(tf.square(self.std)+1e-6)  ) # small reg term to prevent NaNs
-	self.loss = tf.reduce_mean( .5 * tf.exp(-self.std) *tf.square( (self.mean-self.images) ) + .5 * self.std) # small reg term to prevent NaNs
+	self.loss1 = tf.reduce_mean( tf.exp(-self.log_var) *tf.square( (self.mean-self.images) ) )
+	self.loss2 = tf.reduce_mean(self.log_var)
 	# "From What Uncertainties Do We Need in Bayesian Deep Learning for Computer Vision?" NIPS 2017
 	# In practice, we train the network to predict the log variance
-	# i.e. self.std is actually log(sigma^2)
+	self.loss = .5*(self.loss1+self.loss2)
 
 	if self.mode == 'train':
 	    # training stuff
@@ -78,12 +78,14 @@ class Model(object):
 	
 	# summary op
 	loss_summary = tf.summary.scalar('loss', self.loss)
-	log_var_summary = tf.summary.scalar('mean_log_var', tf.reduce_mean(self.std))
+	mean_summary = tf.summary.scalar('loss', self.loss1)
+	log_var_summary = tf.summary.scalar('mean_log_var', self.loss2)
 	image_summary = tf.summary.image('images', self.images)
 	rec_image_summary = tf.summary.image('rec_images', self.mean)
-	uncertainty_summary = tf.summary.image('uncertainty', self.std)
+	uncertainty_summary = tf.summary.image('uncertainty', self.log_var)
 	
 	self.summary_op = tf.summary.merge([loss_summary, \
+					    mean_summary, \
 					    log_var_summary, \
 					    image_summary, \
 					    rec_image_summary, \
